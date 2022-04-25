49b05207a72017898a20ef5eee213fe4e8a4d710
basti <bastian.feil@htomail.de>
2022-04-25 14:59:17 +0200

    a

--- src/Wissensmodi/Konversation/Components/Bubble.js
+++ src/Wissensmodi/Konversation/Components/Bubble.js
@@ -5,7 +5,6 @@ import img2 from './../../../Resources/images/avatarmitkreis.png';
 
 
 const Bubble = () => (
-    <div className="theoretic-conversation">
         <div className="bubble-right">
             <img src={img2} alt='Avatar' />
             <p>Textfeld</p>
@@ -13,14 +12,7 @@ const Bubble = () => (
             <p>Textfeld</p>
             <p>Textfeld</p>
         </div>
-        <div className="bubble-left">
-            <img src={img2} alt='Avatar' />
-            <p>Textfeld</p>
-            <p>Textfeld</p>
-            <p>Textfeld</p>
-            <p>Textfeld</p>
-        </div>
-    </div>
+
 
 );
 export default Bubble;
