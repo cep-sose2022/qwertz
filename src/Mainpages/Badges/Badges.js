201aa5232725bbd2f42a2b0d1ecf654eb938c6aa
Yaren Selvi <yarenselvi@outlook.de>
2022-05-02 22:31:58 +0200

    Headline eingefügt

--- src/Mainpages/Badges/Badges.js
+++ src/Mainpages/Badges/Badges.js
@@ -1,12 +1,13 @@
 import React from "react";
 import BadgeLine from "./components/BadgeLine";
 import "./Badges.css"
-
+import Headline from "../Startseite/Headline";
 const Badges = () => (
     <div className="bodyForBadge">
-        <BadgeLine/>
+        <Headline title={"Badges"} headline={"irgendwas sinnvolles steht hier"} text={""} />
+        <BadgeLine />
     </div>
-  
+
 );
 
 export default Badges;
