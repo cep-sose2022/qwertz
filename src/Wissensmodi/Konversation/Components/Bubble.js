e668c8e1cda5219209353a047bbb43f8721ddd4b
basti <bastian.feil@htomail.de>
2022-05-03 12:53:00 +0200

    konversation überarbeitet

--- src/Wissensmodi/Konversation/Components/Bubble.js
+++ src/Wissensmodi/Konversation/Components/Bubble.js
@@ -1,25 +1,29 @@
 import React from "react";
 import "../Konversation.css";
 
-
-import img2 from './../../../Resources/images/avatarmitkreis.png';
+import img1 from './../../../Resources/images/erlenmeyerkolben_gruen_normal.png';
+import img2 from './../../../Resources/images/erlenmeyerkolben_normal.png';
 
 const Bubble = ({text, category}) => {
     let element;
 
     if (category === "bubble-right") {
+        // chatelemet mit Image links
         element = <div className={category + "-msg-row"}>
             <div className={category + "-msg-text"}>
-                <h2>Name1</h2>
                 <p>{text}</p>
             </div>
-            <img src={img2} alt='Avatar' className={category + "-msg-img"}/>
+            <img src={img1} alt='Avatar' className={category + "-msg-img"}/>
         </div>
     } else {
+        // chatelemet mit Image rechts
         element = <div className={category + "-msg-row"}>
-            <img src={img2} alt='Avatar' className={category + "-msg-img"}/>
+
+                    <img src={img2} alt='Avatar' className={category + "-msg-img"}/>
+
+
             <div className={category + "-msg-text"}>
-                <h2>Name1</h2>
+                {/*<h2>Name1</h2>*/}
                 <p>{text}</p>
             </div>
         </div>
--- src/Wissensmodi/Konversation/Konversation.js
+++ src/Wissensmodi/Konversation/Konversation.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import "./Konversation.css"
+import {Button, Tooltip} from "@mantine/core";
+
 
 import Data from "../../Resources/Json/KonversationData.json";
 import Bubble from "./Components/Bubble";
@@ -7,6 +9,7 @@ import Bubble from "./Components/Bubble";
 
 const Konversation = () => {
     const [bubbles, setBubbles] = useState([]);
+    let allRight = false;
 
 
     if (bubbles[0] === undefined) {
@@ -22,31 +25,51 @@ const Konversation = () => {
 
     }
 
+
     const abbilden = () => {
         const nextBubble = bubbles.filter(bubble => bubble.selected === false)[0];
         nextBubble.selected = true;
         setBubbles(bubbles.filter(bubble => bubble.id !== nextBubble.id).concat(nextBubble));
-
     }
     return (
-        <div className="theoretic-conversation">
-            <div className="chat-objects">
-                {
-                    bubbles.filter(bubbles => bubbles.selected === true).map(
-                        bubbles => <Bubble key={bubbles.id} category={bubbles.category} text={bubbles.text}/>
-                    )
-                }
+        <div>
+            <div className="theoretic-conversation">
 
-                {
-                    bubbles.filter(bubbles => bubbles.selected === false) >= 0 ?
-                        null :
-                        <button onClick={abbilden} className="loading-button"><span>.</span><span>.</span><span>.</span>
-                        </button>
-                }
+                <div className="theoretic-conversation-body">
+
+                    <div className="chat-objects">
+                        {
+                            bubbles.filter(bubbles => bubbles.selected === true).map(
+                                bubbles => <Bubble key={bubbles.id} category={bubbles.category} text={bubbles.text}/>
+                            )
+                        }
+
+                        {
+                            bubbles.filter(bubbles => bubbles.selected === false) >= 0 ?
+                                allRight = true :
+                                <button onClick={abbilden} className="loading-button" id="loading-Button">
+                                    <span>.</span><span>.</span><span>.</span>
+                                </button>
+
+                        }
+                    </div>
+                </div>
+                <div className="theoretic-conversation-footer">
+                    <Tooltip disabled={allRight} label="Du musst die Einheit erst abschließen um weiter zu machen!">
+                        <Button disabled={!allRight}>Weiter</Button>
+                    </Tooltip>
+                </div>
+
             </div>
+
+
         </div>
+
     )
+
 
 };
 
 export default Konversation;
+
+
