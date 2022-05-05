a103555e826a68731034ae677ba74a046ec96c8d
Yaren Selvi <yarenselvi@outlook.de>
2022-05-05 23:48:02 +0200

    Für Fortschrittsanzeige eine eigene css datei erstellt

--- src/Gamemodi/Fortschrittsanzeige.tsx
+++ src/Gamemodi/Fortschrittsanzeige.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import '../Style/Fortschrittsanzeige.css'
 
 const Fortschrittsanzeige = (props: { modis: any; }) => {
 
-    const {modis} = props;
+    const { modis } = props;
     return (
         <>
-            <ul className="progressbar">
-                {
-                    modis.map((modi: { passed: any; title: boolean }) => (
-                        <li key={Math.random()} className={modi.passed ? 'bestanden' : undefined}>{modi.title}</li>
-                    ))
-                }
-            </ul>
+            <div className="wrpvertikal">
+                <ul className="progressbar">
+                    {
+                        modis.map((modi: { passed: any; title: boolean }) => (
+                            <li key={Math.random()} className={modi.passed ? 'bestanden' : undefined}>{modi.title}</li>
+                        ))
+                    }
+                </ul>
+            </div>
         </>
     );
 }
