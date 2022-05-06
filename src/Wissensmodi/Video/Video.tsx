a3b62b4463375ac24f8150839a793f642431e2ff
Yaren Selvi <yarenselvi@outlook.de>
2022-05-06 11:06:31 +0200

    Beispiel Video eingefügt, funktioniert aber noch nicht

--- src/Wissensmodi/Video/Video.tsx
+++ src/Wissensmodi/Video/Video.tsx
@@ -1,15 +1,19 @@
-import React, {useContext} from "react";
-import {ModiContext} from "../../Gamemodi/Gamemodi";
-import {Button} from "@mantine/core";
+import React, { useContext } from "react";
+import { ModiContext } from "../../Gamemodi/Gamemodi";
+import { Button } from "@mantine/core";
+
 
 const Video = () => {
 
     // @ts-ignore
-    const {markAsPassed} = useContext(ModiContext);
+    const { markAsPassed } = useContext(ModiContext);
 
     return (
         <>
-            <h1>Video</h1>
+            <video className="responsive-video" src="Bsp_Video.mp4" controls width="640" height="400"> </video>
+            <video src="Bsp_Video.ogg"></video>
+
+
             <Button onClick={() => markAsPassed('Video')} disabled={false}> Weiter</Button>
         </>
     );
