c2c67b535b6817879353d54f116bb5841686402b
Yaren Selvi <yarenselvi@outlook.de>
2022-05-16 19:44:35 +0200

    Pfeil eingefügt, muss nur noch angepasst werden

--- src/Gamemodi/Wimmelbild/Wimmelbild.js
+++ src/Gamemodi/Wimmelbild/Wimmelbild.js
@@ -1,8 +1,11 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import JsonData from '../../Resources/Json/WimmelbildData.json';
-import {BackgroundImage, Box, Button, Grid, Modal, Title, Tooltip} from "@mantine/core";
-import {ModiContext} from "../Gamemodi";
-import {FcQuestions} from "react-icons/fc";
+import { BackgroundImage, Box, Button, Grid, Modal, Title, Tooltip } from "@mantine/core";
+import { ModiContext } from "../Gamemodi";
+import { FcQuestions } from "react-icons/fc";
+import { ImCircleRight } from "react-icons/im";
+import { ImCircleLeft } from "react-icons/im";
+
 import './Wimmelbild.css'
 
 const modalData = [
@@ -20,7 +23,7 @@ const Wimmelbild = () => {
     const [openedModal, setModalOpened] = useState(false);
     const [allRight, setAllRight] = useState(false);
 
-    const {markAsPassed} = useContext(ModiContext);
+    const { markAsPassed } = useContext(ModiContext);
 
     // läd die daten aus der DB und schreib sie in eine const
     let id = 1;
@@ -64,44 +67,55 @@ const Wimmelbild = () => {
                             transitionDuration={900}
                             overlayOpacity={0.55}
                             overlayBlur={3}
-                            style={{fontSize: 20}}
+                            style={{ fontSize: 20 }}
                             centered
                             opened={openedModal}
                             onClose={() => {
                                 setModalOpened(false);
                             }}
                         >
-                            <Title size="sm" style={{lineHeight: 2.5, fontSize: 22}}>
+                            <Title size="sm" style={{ lineHeight: 2.5, fontSize: 22 }}>
                                 {modalContent.title}
                             </Title>
                             <p>{modalContent.content}</p>
                         </Modal>
                     </Grid.Col>
 
-                    {/* Weiter Button der nur geht, wenn alles gefunden wurde */}
                     <Grid.Col span={2}>
                         <Tooltip label="Du muss alles gefunden haben um weiter zu machen!">
                             <Button onClick={() => markAsPassed(eigenerName)}
-                                    disabled={!allRight}> Weiter</Button>
+                                disabled={!allRight}> Weiter</Button>
                         </Tooltip>
                     </Grid.Col>
 
-                    {/* Button für die Spielerklärung */}
+                    {/*Wimmelbild nächste / zurück */}
                     <Grid.Col span={2}>
-                        <div style={{textAlign: 'end'}}>
+                        <div style={{ textAlign: 'right' }}>
+                            <Button style={{
+                                background: 'transparent'
+                            }}>
+                                <ImCircleLeft size={20} />
+                                <ImCircleRight size={20} />
+                            </Button>
+                        </div>
+                    </Grid.Col>
+
+
+                    {/* Weiter Button der nur geht, wenn alles gefunden wurde */}
+                    <Grid.Col span={2}>
+                        <div style={{ textAlign: 'end' }}>
                             <Button style={{
                                 background: 'transparent'
                             }} onClick={() => setModalOpened(true)}>
-                                <FcQuestions size={32}/>
+                                <FcQuestions size={32} />
                             </Button>
                         </div>
                     </Grid.Col>
 
                 </Grid>
             </div>
-
             <div className="wimmelbild-body">
-                <Box sx={{Width: 500, High: 500}}>
+                <Box sx={{ Width: 500, High: 500 }}>
                     <BackgroundImage
                         style={{
                             zIndex: 1,
@@ -117,27 +131,27 @@ const Wimmelbild = () => {
                         {/*Her kommen alle Buttons rein*/}
                         {
                             buttons.map(b => (
-                                    <button
-                                        key={Math.random()}
-                                        style={{
-                                            border: 'gray',
-                                            opacity: b.isClicked ? .5 : 2,
-                                            backgroundColor: b.isClicked ? 'gray' : 'transparent',
-                                            width: b.width + "%",
-                                            height: b.height + "%",
-                                            position: 'absolute',
-                                            left: `calc(${b.left}%)`,
-                                            top: `calc(${b.top}%)`,
-                                        }}
-                                        onClick={() => clickButton(b.id)}
-                                    />
-                                )
+                                <button
+                                    key={Math.random()}
+                                    style={{
+                                        border: 'gray',
+                                        opacity: b.isClicked ? .5 : 2,
+                                        backgroundColor: b.isClicked ? 'gray' : 'transparent',
+                                        width: b.width + "%",
+                                        height: b.height + "%",
+                                        position: 'absolute',
+                                        left: `calc(${b.left}%)`,
+                                        top: `calc(${b.top}%)`,
+                                    }}
+                                    onClick={() => clickButton(b.id)}
+                                />
+                            )
                             )
                         }
                     </BackgroundImage>
                 </Box>
             </div>
-        </div>
+        </div >
     );
 };
 
