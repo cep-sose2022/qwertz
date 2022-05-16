1b46f328af2c3bd035027041f243915bf422a387
m.heiss <unconfigured@null.spigotmc.org>
2022-05-16 19:22:20 +0200

    Kommentare in Konversation hinzugefügt

--- src/Wissensmodi/Konversation/Konversation.js
+++ src/Wissensmodi/Konversation/Konversation.js
@@ -1,22 +1,35 @@
-import React, { useContext, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "./Konversation.css";
-import { Button, Tooltip, Grid, Modal, Title, Popover, Text } from '@mantine/core';
+import {Button, Tooltip, Grid, Modal, Title, Popover, Text} from '@mantine/core';
 
 
 import Data from "../../Resources/Json/KonversationData.json";
 import Bubble from "./Components/Bubble";
-import { ModiContext } from "../../Gamemodi/Gamemodi";
+import {ModiContext} from "../../Gamemodi/Gamemodi";
+import storage from "../../storage";
+import {useNavigate} from "react-router";
+import {FcQuestions} from "react-icons/fc";
+
+const modalData = [
+    {
+        title: "Spielerklärung",
+        content: "Klick einfach un les " //TODO sinnvoller text
+    }
+]
 
 const Konversation = () => {
+    const eigenerName = "Konversation";
     const [bubbles, setBubbles] = useState([]);
     const [buttons, setButton] = useState([]);
-    const [modalContent, setModalContent] = useState('');
+    const [modalContent, setModalContent] = useState(modalData[0]);
     const [openedModal, setModalOpened] = useState(false);
     const [setAllRight] = useState(false);
-    let allRight = false;
+    let allRight = true;
+    const navigator = useNavigate();
 
-    const { markAsPassed } = useContext(ModiContext);
+    const {markAsPassed} = useContext(ModiContext);
 
+    // läd die daten aus der DB und schreib sie in eine const
     if (bubbles[0] === undefined) {
         Data.map(object => {
             let bubble = {
@@ -27,26 +40,25 @@ const Konversation = () => {
             }
             bubbles.push(bubble);
         })
-
     }
-    const modalContent1 = [
-        {
-            title: "Aufgabenstellung",
-            content: "hier hast du eine Wichtige und coole aufgabe"
-        },
-        {
-            title: "Leider Falsch",
-            content: "Leider nicht alles richtig, schaue dir das noch mal an."
-        },
-        {
-            title: "Alles Richtig",
-            content: "Super du hast alles richtig!"
-        }, {
-            title: "Spielerklärung",
-            content: "Ordne die Elemente den zwei Verschiedenen Boxen zu. "
-        }
-    ]
 
+    // useEffect(() =>
+    //     redirect()
+    // )
+
+    // navigierte zum aktuellen Modi
+    // const [firstRender, setFirstRender] = useState(true)
+    // const redirect = () => {
+    //     if (firstRender) {
+    //         setFirstRender(false)
+    //         const currentModiTitle = storage.getCurrentModiTitle()
+    //         if (currentModiTitle !== null && currentModiTitle !== eigenerName) {
+    //             navigator('../' + currentModiTitle)
+    //         }
+    //     }
+    // }
+
+    // zum anzeigen des Nächsten Textes
     const abbilden = () => {
         const nextBubble = bubbles.filter(bubble => bubble.selected === false)[0];
         if (nextBubble !== undefined) {
@@ -54,107 +66,88 @@ const Konversation = () => {
             setBubbles(bubbles.filter(bubble => bubble.id !== nextBubble.id).concat(nextBubble));
         }
     }
+
     return (
         <div className="konversation-container">
             <div className="konversation-header">
-                <Grid justify={"center"} >
+                <Grid justify={"space-between"}>
                     <Grid.Col span={2}>
-
                         <Modal
                             transition="slide-down"
                             transitionDuration={900}
-                            // transitionTimingFunction="ease"
                             overlayOpacity={0.55}
                             overlayBlur={3}
-                            style={{ fontSize: 20 }}
-                            // bgColor='red'
+                            style={{fontSize: 20}}
                             centered
                             opened={openedModal}
                             onClose={() => {
                                 setModalOpened(false);
                             }}
-
                         >
-
-                            <Title size="sm" style={{ lineHeight: 2.5, fontSize: 22 }}>
-                                {modalContent}
+                            <Title size="sm" style={{lineHeight: 2.5, fontSize: 22}}>
+                                {modalContent.title}
                             </Title>
-
-                            <p>{modalContent}</p>
+                            <p>{modalContent.content}</p>
                         </Modal>
-                        <Button onClick={() => {
-                            setModalContent("test")
-                            setModalOpened(true)
-                        }}>Aufgabenstellung</Button>
                     </Grid.Col>
 
+                    {/* Weiter Button der nur geht, wenn alles gelesen wurde */}
                     <Grid.Col span={2}>
-                        <Popover
-                            target={<Button onClick={"test"}>Fertig</Button>}
-                            width={260}
-                            position="bottom"
-                            withArrow
-                        >
-                            <div style={{ display: 'flex' }}>
-                                <Text size="sm">Du musst erst alle Boxen einsetzen</Text>
-                            </div>
-                        </Popover>
-
-                        <Tooltip label="Du muss alles richtig haben um weiter zu machen!">
-                            <Button onClick={() => markAsPassed('Konversation')}
-                                disabled={!allRight}> Weiter</Button>
+                        <Tooltip label="Du muss alles gelesen haben um weiter zu machen!">
+                            <Button onClick={() => markAsPassed(eigenerName)}
+                                    disabled={!allRight}> Weiter</Button>
                         </Tooltip>
-
-
                     </Grid.Col>
 
+                    {/* Button für die Spielerklärung */}
+                    <Grid.Col span={2}>
+                        <div style={{textAlign: 'end'}}>
+                            <Button style={{
+                                background: 'transparent'
+                            }} onClick={() => setModalOpened(true)}>
+                                <FcQuestions size={32}/>
+                            </Button>
+                        </div>
+                    </Grid.Col>
                 </Grid>
             </div>
 
             <div className="konversation-body">
-                <div>
-                    <div className="theoretic-conversation" onClick={abbilden}>
-                        <div className="theoretic-conversation-body">
-                            <div className="chat-window">
-                                <div className="top-menu">
-                                    <div className="buttons">
-                                        <div className="button close" />
-                                        <div className="button minimize" />
-                                        <div className="button maximize" />
-                                    </div>
-                                    <div className="title-chat">
-                                        <p>Chat</p>
-                                    </div>
-
+                <div className="theoretic-conversation" onClick={abbilden}>
+                    <div className="theoretic-conversation-body">
+                        <div className="chat-window">
+                            <div className="top-menu">
+                                <div className="buttons">
+                                    <div className="button close"/>
+                                    <div className="button minimize"/>
+                                    <div className="button maximize"/>
                                 </div>
-
+                                <div className="title-chat">
+                                    <p>Chat</p>
+                                </div>
                             </div>
+                        </div>
 
+                        <div className="chat-objects">
+                            {
+                                bubbles.filter(bubbles => bubbles.selected === true).map(
+                                    bubbles => <Bubble key={bubbles.id} category={bubbles.category}
+                                                       text={bubbles.text}/>
+                                )
+                            }
 
-                            <div className="chat-objects">
-                                {
-                                    bubbles.filter(bubbles => bubbles.selected === true).map(
-                                        bubbles => <Bubble key={bubbles.id} category={bubbles.category}
-                                            text={bubbles.text} />
-                                    )
-                                }
+                            {
+                                bubbles.filter(bubbles => bubbles.selected === false) >= 0 ?
+                                    allRight = true :
+                                    <button className="loading-button" id="loading-Button">
+                                        <span>.</span><span>.</span><span>.</span>
+                                    </button>
 
-                                {
-                                    bubbles.filter(bubbles => bubbles.selected === false) >= 0 ?
-                                        allRight = true :
-                                        <button className="loading-button" id="loading-Button">
-                                            <span>.</span><span>.</span><span>.</span>
-                                        </button>
+                            }
+                        </div>
 
-                                }
-                            </div>
-
-                            <div className="bottom_wrapper">
-                                <textarea className="message_input" placeholder="type a message" />
-                                <Tooltip disabled={allRight} label="Du musst die Einheit erst abschließen um weiter zu machen!">
-                                    <Button onClick={() => markAsPassed('Konversation')} disabled={!allRight}> Weiter</Button>
-                                </Tooltip>
-                            </div>
+                        <div className="bottom_wrapper">
+                            <textarea className="message_input" placeholder="type a message"/>
                         </div>
                     </div>
                 </div>
