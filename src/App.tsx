870e8a66361e01fba513b1bccf621e89211f58e2
basti <bastian.feil@htomail.de>
2022-05-16 14:53:26 +0200

    Dashboard Bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import Startseite from './Mainpages/Startseite/Startseite';
 import Dashboard from './Mainpages/Dashboard/Dashboard';
 import Badges from './Mainpages/Badges/Badges';
+import Sammlung from './Mainpages/Sammlung/Sammlung';
 
 // import Gamemodi
 import Gamemodi from './Gamemodi/Gamemodi';
@@ -32,7 +33,7 @@ function App() {
                 <a> Zum Debuggen und Testen aller Seiten, kommt dann am Ende noch weg</a>
                 <br/>
                 <Link to='Startseite'>Startseite </Link>
-                <Link to='Dashboard'>Dashboard </Link>
+                <Link to='Dashboard/Sammlung'>Dashboard </Link>
                 <Link to='Dashboard/Badges'>Badges </Link>
                 <Link to='Dashboard/Gamemodi/Zuordnung'>Zuordnung </Link>
                 <Link to='Dashboard/Gamemodi/Ablaufanordnung'>Ablaufanordnung </Link>
@@ -49,6 +50,7 @@ function App() {
                 {/*Dashboard un alles was darunter ist }*/}
                 <Route path="Dashboard" element={<Dashboard/>}>
                     <Route path="Badges" element={<Badges/>}/>
+                    <Route path="Sammlung" element={<Sammlung/>}/>
                     <Route path="Error503" element={<Error503Page/>}/>
                     <Route path="Gamemodi" element={<Gamemodi/>}>
                         {/*Alles was unter Gamemodi ist*/}
