33892ff12a76f58a00d91a2d1a9c08bd173005bf
basti <bastian.feil@htomail.de>
2022-05-17 11:05:39 +0200

    Badges angepasst

--- src/Mainpages/Badges/Badges.js
+++ src/Mainpages/Badges/Badges.js
@@ -4,18 +4,23 @@ import "./Badges.css"
 import "./Schloss.css"
 
 import Headline from "../Startseite/Headline";
-import { BackgroundImage } from "@mantine/core";
+import {BackgroundImage} from "@mantine/core";
 
 import JsonData from "../../Resources/Json/BadgeData.json";
 
 const Badges = () => (
-    <div className="bodyForBadge">
-        <Headline title={"Badges"} headline={"Dein Fortschritt"} text={""} />
-        <BackgroundImage className="image" src={''}>
-            <BadgeLine badgeData={JsonData} />
-        </BackgroundImage>
-    </div>
+        <div className="badges-container">
+            <div className="badges-header">
+                <Headline title={"Badges"} headline={"Dein Fortschritt"} text={""}/>
+            </div>
+            <div className="badges-body">
+                <BackgroundImage className="image" src={''}>
+                    <BadgeLine badgeData={JsonData}/>
+                </BackgroundImage>
+            </div>
+        </div>
 
-);
+    )
+;
 
 export default Badges;
--- src/Mainpages/Badges/components/BadgeLine.js
+++ src/Mainpages/Badges/components/BadgeLine.js
@@ -3,15 +3,6 @@ import BadgeItem from "./BadgeItem";
 
 const BadgeLine = (props) => {
     const {badgeData} = props;
-
-    // BadgeData.length > 0 && (
-    //     <div className="badgeline-container">
-    //         {BadgeData.map((data, idx) => (
-    //             <BadgeItem data={data} key={idx}/>
-    //
-    //         ))}
-    //     </div>
-
     return (
         <div className="badgeline-container">
             {
