d5327baeea8e6f6fc1f5bdf48d500fd5fefe0a52
basti <bastian.feil@htomail.de>
2022-05-24 11:41:18 +0200

    Badges pdf download hinzugefügt

--- src/Mainpages/Badges/components/BadgeItem.js
+++ src/Mainpages/Badges/components/BadgeItem.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import storage from "../../../storage";
+import img1 from "../../../Resources/images/orange_zwinkern.png";
+import {FaFileDownload} from "react-icons/fa";
+import {Tooltip} from "@mantine/core";
 
 const setData = (badgeID) => {
     storage.setBadgeID(badgeID)
@@ -18,11 +21,26 @@ const BadgeItem = ({data}) => (
                 </span>
                     <h3>{data.title}</h3>
                     <p>{data.text}</p>
-                    <Link to={'../Gamemodi/' + data.modis[0]} onClick={()=>setData(data.badgeID)}>
-                        Start
-                    </Link>
+
+                    <div
+                        style={{width: "100%", display: 'flex', flexDirection: "row", justifyContent: "space-between"}}>
+                        <div>
+                            <Link className="link" to={'../Gamemodi/' + data.modis[0]}
+                                  onClick={() => setData(data.badgeID)}>
+                                Start
+                            </Link>
+                        </div>
+                        <div>
+                            <Tooltip label="Hier kannst du dir den Inhalt dieses Badges in einer PDF-Datei herunterladen.">
+                                <a className="downloadButton" href={img1} download>
+                                    <FaFileDownload/>
+                                </a>
+                            </Tooltip>
+                        </div>
+                    </div>
                     <span className="circle"/>
                 </div>
+
                 :
                 <div className="badgeline-item-content">
                 <span className="tag">
@@ -31,10 +49,29 @@ const BadgeItem = ({data}) => (
                     <h3>{data.title}</h3>
                     <p style={{filter: 'blur(4px)'}}>{data.text}</p>
 
-
-                    <Link disabled='disabled' style={{filter: 'blur(4px)'}} to={''}>
-                        Start
-                    </Link>
+                    <div
+                        style={{
+                            width: "100%",
+                            display: 'flex',
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            filter: 'blur(4px)'
+                        }}>
+                        <div>
+                            <Tooltip disabled='disabled'
+                                     label="Hier kannst du dir den Inhalt dieses Badges in einer PDF-Datei herunterladen.">
+                                <a className="downloadButton">
+                                    <FaFileDownload/>
+                                </a>
+                            </Tooltip>
+                        </div>
+                        <div>
+                            <Link disabled='disabled' className="link" to={''}
+                                  onClick={() => setData(data.badgeID)}>
+                                Start
+                            </Link>
+                        </div>
+                    </div>
                     <span className="circle"/>
                 </div>
         }
