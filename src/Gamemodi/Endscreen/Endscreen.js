624de7f61f951cb0474c58daad37f7a6e9062577
m.heiss <unconfigured@null.spigotmc.org>
2022-05-24 11:31:52 +0200

    Endscreen umgebaut

--- src/Gamemodi/Endscreen/Endscreen.js
+++ src/Gamemodi/Endscreen/Endscreen.js
@@ -1,10 +1,66 @@
 import React from "react";
-import EndscreenPlace from "./components/EndscreenPlace";
 import "./Endscreen.css";
+import {Container, Grid} from "@mantine/core";
+import {Link} from "react-router-dom";
+import {VscFolderLibrary} from "react-icons/vsc";
+import {FaAward} from "react-icons/fa";
+import EndscreenData from "./EndscreenData";
+import EndscreenItem from "./components/EndscreenItem";
+import image from "../../Resources/images/orange_zwinkern.png";
+import gif from "../../Resources/zeppgefunden.gif";
+import storage from "../../storage";
 
 const Endscreen = () => {
+    const machEnde = () => {
+        let badgeID = storage.getBadgeID()
+        storage.setBadgePassed(parseInt(badgeID))
+        storage.removeAll()
+    }
+
     return (
-        <EndscreenPlace />
+        <div className="endscreen-container">
+            <div className="endscreen-header">
+                <Grid justify={"space-around"}>
+                    <div className="btn" onClick={machEnde}>
+                        <Link to="../../Sammlung">
+                        <span style={{fontSize: '20px', textAlign: 'center', itemsAlign: 'center'}}>
+                            <VscFolderLibrary/> Weiter zur Sammlung
+                        </span>
+                        </Link>
+                    </div>
+                    <div className="btn" onClick={machEnde}>
+                        <Link to="../../Badges" style={{fontSize: '20px', textAlign: 'center', itemsAlign: 'center'}}>
+                            <FaAward/> Zu den Badges </Link>
+                    </div>
+                </Grid>
+            </div>
+            <div className="endscreen-body">
+                <Grid justify={"center"}>
+                    <div className="endscreen-text">
+                        <div className="speechbubble">
+                            {EndscreenData.map((data, idx) => (
+                                <EndscreenItem data={data} key={idx}/>
+                            ))}
+                        </div>
+                    </div>
+
+
+                    <img className="imgerlen" src={image} alt="erlenmeyerkolbenZwinkert"/>
+                </Grid>
+
+            </div>
+            <>
+                <Container size={10100} px={0}>
+                    <section className="endscreengif">
+                        <img className="gif" src={gif} alt=""/>
+                    </section>
+                </Container>
+
+            </>
+
+
+        </div>
+
     )
 };
 
--- src/Gamemodi/Endscreen/components/EndscreenItem.js
+++ src/Gamemodi/Endscreen/components/EndscreenItem.js
@@ -4,9 +4,6 @@ const EndscreenItem = ({ data }) => (
     <div className="endscreen-item">
         <div className="endscreen-item-content">
             <p>{data.text}</p>
-
-
-
         </div>
     </div>
 )
