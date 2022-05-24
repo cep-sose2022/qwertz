d161aa1dab4ab0bd9e81e2fffeb28e14598756e9
basti <bastian.feil@htomail.de>
2022-05-24 10:10:03 +0200

    MultipleChoice design erstellt

--- src/Wissensmodi/MultipleChoice/Components/MultipleChoiceField.js
+++ src/Wissensmodi/MultipleChoice/Components/MultipleChoiceField.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Center, Checkbox} from "@mantine/core";
 
 const MultipleChoiceField = (props) => {
 
@@ -6,30 +7,38 @@ const MultipleChoiceField = (props) => {
 
     const handleChange = (event) => {
         console.log(event.target.name)
+        console.log(event.target.value)
     }
 
     return (
-        <>
-            <div key={Math.random()}>
-                <h1>{aufgabe.frage}</h1>
-                <ul key={Math.random()}>
-                    {
-                        aufgabe.antworten.map(antwort => (
-                                <li key={Math.random()}>
-                                    <input
-                                        type="radio"
-                                        name={aufgabe.frage}
-                                        value={antwort.text}
-                                        onChange={handleChange}
-                                    />
-                                    <label>{antwort.text}</label>
-                                </li>
-                            )
-                        )
-                    }
-                </ul>
-            </div>
-        </>
+        <div className="multiBox">
+            <Center>
+                <div key={Math.random()}>
+                    <div className="multiQuestion">
+                        <h3>{aufgabe.frage}</h3>
+                    </div>
+                    <div className="multiAnswers">
+                        <ul key={Math.random()}>
+                            {
+                                aufgabe.antworten.map(antwort => (
+                                        <li key={Math.random()}>
+                                            <Checkbox
+                                                // icon={}
+                                                label={antwort.text}
+                                                name={aufgabe.frage}
+                                                value={antwort.text}
+                                                onChange={handleChange}
+                                            />
+                                        </li>
+                                    )
+                                )
+                            }
+                        </ul>
+
+                    </div>
+                </div>
+            </Center>
+        </div>
     )
 }
 
