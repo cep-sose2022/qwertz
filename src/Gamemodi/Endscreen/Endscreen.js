628770997e25330a72028d18b285d25b3433375f
Yaren Selvi <yarenselvi@outlook.de>
2022-05-30 18:55:57 +0200

    Opening Box Animation dynamischer gemacht, fehlt nur noch die Logik für die einzelnen Badges

--- src/Gamemodi/Endscreen/Endscreen.js
+++ src/Gamemodi/Endscreen/Endscreen.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Endscreen.css";
-import {Container, Grid} from "@mantine/core";
-import {Link} from "react-router-dom";
-import {VscFolderLibrary} from "react-icons/vsc";
-import {FaAward} from "react-icons/fa";
+import { Container, Grid } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { VscFolderLibrary } from "react-icons/vsc";
+import { FaAward } from "react-icons/fa";
 import EndscreenData from "./EndscreenData";
 import EndscreenItem from "./components/EndscreenItem";
 import image from "../../Resources/images/orange_zwinkern.png";
-import gif from "../../Resources/zeppgefunden.gif";
+import gif from "../../Resources/mysterybox-anim.gif";
 import storage from "../../storage";
+import JsonList from '../../Resources/Json/BelohnungData.json';
 
 const Endscreen = () => {
+    const image = require('../../Resources/images/' + JsonList[0].bild);
+
+    const imagebox = require('../../Resources/images/' + JsonList[1].bildBox);
+
     const machEnde = () => {
         storage.setBadgePassed(storage.getBadgeID())
         storage.removeAll()
@@ -22,14 +27,14 @@ const Endscreen = () => {
                 <Grid justify={"space-around"}>
                     <div className="btn" onClick={machEnde}>
                         <Link to="../../Sammlung">
-                        <span style={{fontSize: '20px', textAlign: 'center', itemsAlign: 'center'}}>
-                            <VscFolderLibrary/> Weiter zur Sammlung
-                        </span>
+                            <span style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center' }}>
+                                <VscFolderLibrary /> Weiter zur Sammlung
+                            </span>
                         </Link>
                     </div>
                     <div className="btn" onClick={machEnde}>
-                        <Link to="../../Badges" style={{fontSize: '20px', textAlign: 'center', itemsAlign: 'center'}}>
-                            <FaAward/> Zu den Badges </Link>
+                        <Link to="../../Badges" style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center' }}>
+                            <FaAward /> Zu den Badges </Link>
                     </div>
                 </Grid>
             </div>
@@ -38,20 +43,21 @@ const Endscreen = () => {
                     <div className="endscreen-text">
                         <div className="speechbubble">
                             {EndscreenData.map((data, idx) => (
-                                <EndscreenItem data={data} key={idx}/>
+                                <EndscreenItem data={data} key={idx} />
                             ))}
                         </div>
                     </div>
 
+                    <img className="imgerlen" src={imagebox} alt="erlenmeyerkolbenZwinkert" />
 
-                    <img className="imgerlen" src={image} alt="erlenmeyerkolbenZwinkert"/>
+                    <img className="imgerlenBox" src={image} alt="erlenmeyerkolbenZwinkert" />
                 </Grid>
 
             </div>
             <>
                 <Container size={10100} px={0}>
                     <section className="endscreengif">
-                        <img className="gif" src={gif} alt=""/>
+                        <img className="gif" src={gif} alt="" />
                     </section>
                 </Container>
 
