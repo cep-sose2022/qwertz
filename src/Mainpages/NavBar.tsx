ae260043c0f3db1228bfbd105a290b80c590bdfe
basti <bastian.feil@htomail.de>
2022-05-31 10:39:09 +0200

    Icon geändert

--- src/Mainpages/NavBar.tsx
+++ src/Mainpages/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import erlenmeyer from '../Resources/erlenmeyer_animiert.gif';
+import erlenmeyer from '../Resources/erlenmeyer-animiert2-unscreen.gif';
 import { Link } from "react-router-dom";
 
 const NavBar = (props: { children: any; }) => {
