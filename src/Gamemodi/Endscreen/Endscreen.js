199b37030f68179c3f42a2da0dafebc8a7ef9660
m.heiss <unconfigured@null.spigotmc.org>
2022-06-10 09:46:58 +0200

    Textfarbe auf Schwarz geändert

--- src/Gamemodi/Endscreen/Endscreen.js
+++ src/Gamemodi/Endscreen/Endscreen.js
@@ -33,13 +33,13 @@ const Endscreen = () => {
                 <Grid justify={"space-around"}>
                     <div className="btn" onClick={machEnde}>
                         <Link to="../../Sammlung">
-                            <span style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center' }}>
+                            <span style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center', color: "black"}}>
                                 <VscFolderLibrary /> Weiter zur Sammlung
                             </span>
                         </Link>
                     </div>
                     <div className="btn" onClick={machEnde}>
-                        <Link to="../../Badges" style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center' }}>
+                        <Link to="../../Badges" style={{ fontSize: '20px', textAlign: 'center', itemsAlign: 'center', color: "black" }}>
                             <FaAward /> Zu den Badges </Link>
                     </div>
                 </Grid>
