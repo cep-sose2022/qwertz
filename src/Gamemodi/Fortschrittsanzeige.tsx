dfa22b60a8125d259822f6bd2ea1afdaf98f4e1f
m.heiss <unconfigured@null.spigotmc.org>
2022-06-18 20:04:39 +0200

    Rechtschreibfehler Multiple-Choice behoben

--- src/Gamemodi/Fortschrittsanzeige.tsx
+++ src/Gamemodi/Fortschrittsanzeige.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 import '../Style/Fortschrittsanzeige.css'
 
 // import Mantine Core
-import { Stepper } from '@mantine/core';
+import {Stepper} from '@mantine/core';
 
 
 const Fortschrittsanzeige = (props: { modis: any; }) => {
-    const { modis } = props;
+    const {modis} = props;
     let bestandeneModi = 0;
     modis.map((modi: { passed: boolean; }) => modi.passed ? bestandeneModi++ : null)
 
@@ -17,7 +17,8 @@ const Fortschrittsanzeige = (props: { modis: any; }) => {
             <Stepper active={bestandeneModi} breakpoint="sm">
                 {
                     modis.map((modi: { title: string }) =>
-                        <Stepper.Step key={Math.random()} label={modi.title} />
+                        <Stepper.Step key={Math.random()}
+                                      label={modi.title === 'Multiplechoice' ? 'Multiple-Choice' : modi.title}/>
                     )
                 }
             </Stepper>
